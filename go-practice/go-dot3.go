320402277d1ad78687899455ddb8dfe17b134e2f
TaigaMikami <mikamimikami60@gmail.com>
2018-03-17 01:03:12 +0900

    dot3

---
+++ go-practice/go-dot3.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  // var msg string
+  // msg = "hello workd"
+  // var msg = "hello world"
+  msg := "hello world"
+  fmt.Println(msg)
+
+  // var a, b int
+  // a, b := 10, 15
+  //
+  // var (
+  //   c int
+  //   d string
+  // )
+  // c = 20
+  // d = "hoge"
+}
